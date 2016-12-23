100ca91ea8dfcdd4d0572773c26c748f7186c16d
Đăng LV <danglv@topica.edu.vn>
2016-12-23 19:30:19 +0700

    mock api with method post with params require and optional

--- app/controllers/api/v1/homes.rb
+++ app/controllers/api/v1/homes.rb
@@ -6,17 +6,37 @@ module API
       require 'json'
 
       resource :homes do
-        desc "Return all homes"
+        desc "MockAPI return message"
         get "index", root: :homes do
           {
             message: "test"
           }
         end
 
+        desc "MockAPI get call net::http"
         get "/info", root: :homes do
           FAKE_URL = "https://jsonplaceholder.typicode.com/posts"
           uri = URI.parse FAKE_URL
           Net::HTTP.get(uri)
+        end
+
+        desc "MockAPI post with params"
+        params do
+          requires :id, type: Integer, desc: "Mock ID require"
+          requires :token, type: String, desc: "Mock token require"
+          optional :message, type: String, desc: "Mock message option"
+        end
+        post "/mock_post" do
+          message = {
+            id: "you post id = #{permitted_params[:id]}",
+            token: "you post token = #{permitted_params[:token]}"
+          }
+          message[:message] = if permitted_params[:message]
+             "you post message = #{permitted_params[:message]}"
+          else
+            "you not post message"
+          end
+          message
         end
       end
     end
